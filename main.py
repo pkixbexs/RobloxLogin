c550ad012ce07c6790d8b565c3c0985237e8aec3
pkixbexs <babapuroalttantire2302rblxabsi@gmail.com>
2026-04-15 12:12:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,7 @@ def login():
     return redirect("https://www.roblox.com/login")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    # Render portu otomatik atar, o yüzden portu os.environ'dan alıyoruz
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
